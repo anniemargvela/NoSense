218f8ae5113e1ab3d612d5fb2add141cec13e752
anniemargvela <anmar13@freeuni.edu.ge>
2020-10-04 05:01:46 +0400

    NoSenseTask

---
+++ NoSenseTask/NoSenseClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoSenseTask
+{
+    static class NoSenseClass
+    {
+        public static T ThisDoesntMakeAnySense<T>(this IEnumerable<T> list, Func<T, bool> Predicate, Func<T> Creator)
+        {
+            ValidateParameters(list, Predicate, Creator);
+
+            foreach (var elem in list)
+            {
+                if (Predicate(elem)) return elem;
+            }
+
+            return Creator();
+        }
+
+        private static void ValidateParameters<T>(IEnumerable<T> list, Func<T, bool> Predicate, Func<T> Creator)
+        {
+            if (list == null)
+                throw new NullReferenceException("List pointer is NULL");
+            if (Predicate == null)
+                throw new NullReferenceException("Predicate pointer is NULL");
+            if (Creator == null)
+                throw new NullReferenceException("Creator delegate pointer is NULL");
+        }
+    }
+}
---
+++ NoSenseTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace NoSenseTask
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            //If there are extra spaces between integers, Trim is needed 
+            var list = input.Split(",")
+                            .Select(i => int.Parse(i.Trim()));
+
+            var list1 = list.Where(i => i % 2 == 1);
+            Test(list1, 1);
+
+            var list2 = list.Where(i => i % 2 == 0);
+            Test(list2, 2);
+            
+            List<int> list3 = null;
+            Test(list3, 3);
+
+
+        }
+
+        static void Test(IEnumerable<int> list, int testcase)
+        {
+            Console.WriteLine($"Test Case N{testcase}:");
+            if (list != null)
+            {
+                foreach (var i in list)
+                    Console.Write($"{i} ");
+            }
+
+            try
+            {
+                var rv = list.ThisDoesntMakeAnySense(p => p % 2 == 1, () => 777);
+                Console.WriteLine($"=> {rv}");
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
